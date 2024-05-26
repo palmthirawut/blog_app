5c60a46e35724ce4a073699f6b46ec9388e84c8b
thirawut <thirawut.tdev @is gmail.com>
2024-05-26 16:27:14 +0700

    first commit

---
+++ lib/core/secret/app_secret.dart
@@ -0,0 +1,5 @@
+class AppSecrets {
+  static const supabaseUrl = 'https://hxzazrjfwvyvbhusphae.supabase.co';
+  static const supabaseAnonKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh4emF6cmpmd3Z5dmJodXNwaGFlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY3MTI1MDYsImV4cCI6MjAzMjI4ODUwNn0.v4pnuocsL6H0Q2TLphmjchTELYRY3HL01qkLXwTgdNU';
+}
---
+++ lib/init_dependencies.main.dart
@@ -0,0 +1,74 @@
+part of 'init_dependencies.dart';
+
+final serviceLocator = GetIt.instance;
+
+Future<void> initDependencies() async {
+  _initAuth();
+
+  final supabase = await Supabase.initialize(
+    url: AppSecrets.supabaseUrl,
+    anonKey: AppSecrets.supabaseAnonKey,
+  );
+
+  Hive.defaultDirectory = (await getApplicationDocumentsDirectory()).path;
+
+  serviceLocator.registerLazySingleton(() => supabase.client);
+
+  serviceLocator.registerLazySingleton(
+    () => Hive.box(name: 'blogs'),
+  );
+
+  serviceLocator.registerFactory(() => InternetConnection());
+
+  // core
+  serviceLocator.registerLazySingleton(
+    () => AppUserCubit(),
+  );
+  serviceLocator.registerFactory<ConnectionChecker>(
+    () => ConnectionCheckerImpl(
+      serviceLocator(),
+    ),
+  );
+}
+
+void _initAuth() {
+  // Datasource
+  serviceLocator
+    ..registerFactory<AuthRemoteDataSource>(
+      () => AuthRemoteDataSourceImpl(
+        serviceLocator(),
+      ),
+    )
+    // Repository
+    ..registerFactory<AuthRepository>(
+      () => AuthRepositoryImpl(
+        serviceLocator(),
+        serviceLocator(),
+      ),
+    )
+    // Usecases
+    ..registerFactory(
+      () => UserSignUp(
+        serviceLocator(),
+      ),
+    )
+    ..registerFactory(
+      () => UserLogin(
+        serviceLocator(),
+      ),
+    )
+    ..registerFactory(
+      () => CurrentUser(
+        serviceLocator(),
+      ),
+    )
+    // Bloc
+    ..registerLazySingleton(
+      () => AuthBloc(
+        userSignUp: serviceLocator(),
+        userLogin: serviceLocator(),
+        currentUser: serviceLocator(),
+        appUserCubit: serviceLocator(),
+      ),
+    );
+}
